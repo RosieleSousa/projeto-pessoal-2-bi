97194a33988fab881ef6b549a76e58fcee131f67
Rosiele Sousa <rosielesousa49@gmail.com>
2024-11-14 09:25:38 -0300

    up

---
+++ script.js
@@ -0,0 +1,13 @@
+
+do{
+   let option = prompt(`como voce esta se sentindo agora? irei te indicar alguns cantores e suas musicas para voce
+    (escolha uma opcao:)voce esta:
+    1.raiva;
+    2.triste;
+    3.ansioso;
+    4.solitario;
+    5.medo;
+    6.sair.
+    `)
+   
+} while (option !== '6')
