68702cd2efbe9d68a39acf49f0d554d662090d2c
Rosiele Sousa <rosielesousa49@gmail.com>
2024-11-15 13:01:13 -0300

    ⁠☆

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function one( angry ){
-    return angry[`
-        "Sweater Weather" de The Neighbourhood
-        "Everybody Hurts" de REM 
-        "Nothing Compares 2 U" de Sinead O'Connor 
-        "Tears in Heaven" de Eric Clapton 
-        "I Will Always Love You" de Whitney Houston 
-        "Yesterday" dos Beatles 
-        "Someone Like You" de Adele 
-        "My Heart Will Go On" de Celine Dion 
-        "Crying" de Roy Orbison 
-        "Streets Of Philadelphia" de Bruce Springsteen 
-        "Love Will Tear Us Apart" do Joy Division `]
-}alert(angry)
+    return angry[
+        '"Sweater Weather" de The Neighbourhood',
+        '"Everybody Hurts" de REM', 
+        '"Nothing Compares 2 U" de Sinead O Connor', 
+        '"Tears in Heaven" de Eric Clapton', 
+        '"I Will Always Love You" de Whitney Houston',
+        '"Yesterday" dos Beatles', 
+        '"Someone Like You" de Adele', 
+        '"My Heart Will Go On" de Celine Dion',
+        '"Crying" de Roy Orbison', 
+        '"Streets Of Philadelphia" de Bruce Springsteen',
+        '"Love Will Tear Us Apart" do Joy Division' ]
+}(angry)
 function two(sad){
-    return sad[`
-        "cardigan" de Taylor Swift
-        "Fall in Love with You" de Montell Fish
-        "Black Friday" de Tom Odell
-        "Here With Me" de d4vd
-        "Those Eyes" de New West
-        "Deep End" de Holly Humberstone
-        "vampire" de Olivia Rodrigo`]
-}alert(sad)
+    return sad[
+        '"cardigan" de Taylor Swift',
+        '"Fall in Love with You" de Montell Fish',
+        '"Black Friday" de Tom Odell',
+        '"Here With Me" de d4vd',
+        '"Those Eyes" de New West',
+        '"Deep End" de Holly Humberstone',
+        '"vampire" de Olivia Rodrigo']
+}(sad)
 do{
    let option = prompt(`como voce esta se sentindo agora? irei te indicar alguns cantores e suas musicas para voce
     (escolha uma opcao:)voce esta:
